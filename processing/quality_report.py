ab07e5f5b0ab744ac30357dd140a8aca493bf252
MAGNE KUATE Josiane audrey <josianeaudrey.magnekuate@coexya.eu>
2026-02-12 15:04:22 +0100

    Refactor quality report generation: streamline file handling for multiple years and remove obsolete quality report files

--- processing/quality_report.py
+++ processing/quality_report.py
@@ -53,13 +53,18 @@ def generate_quality_report(
 
 
 if __name__ == "__main__":
-    file_path = "data/processed_data/DAT_MT_GBPUSD_M1_2022_processed.csv"
-    output_csv_path = "data/processed_data/DAT_MT_GBPUSD_M15_2022_clean.csv"
-    output_report_path = "data/processed_data/DAT_MT_GBPUSD_M15_2022_quality_report.txt"
+    file_path_list= ["DAT_MT_GBPUSD_M1_2022_processed", "DAT_MT_GBPUSD_M1_2023_processed", "DAT_MT_GBPUSD_M1_2024_processed"]
+    for pat in file_path_list:
+        print()
+        file_path = "data/processed_data/" + pat + ".csv"
+        output_base = pat.replace("_M1_", "_M15_")
+        output_csv_path = "data/processed_data/" + output_base + ".csv"
+        output_report_path = "data/processed_data/" + output_base + "_quality_report.txt"
 
-    report_text = generate_quality_report(
-        file_path,
-        output_csv_path,
-        output_report_path
-    )
-    print(report_text)
+        report_text = generate_quality_report(
+            file_path,
+            output_csv_path,
+            output_report_path
+        )
+        print(report_text)
+  
